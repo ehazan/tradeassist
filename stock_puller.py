de5601f7d6bb802f7f97035498a3dddbe834e9cb
Eugene Hazan <ehazan@gmail.com>
2016-12-05 22:13:47 -0500

    Organized code

--- stock_puller.py
+++ stock_puller.py
@@ -5,13 +5,14 @@ import numpy as np
 import pdb
 
 class StockPuller():
-
     def __init_(self):
         pass
 
     def pull_data(self, stock):
         print('Currently Pulling',stock)
-        url_to_visit = 'http://chartapi.finance.yahoo.com/instrument/1.0/'+stock+'/chartdata;type=quote;range=1y/csv'
+        url_to_visit = 'http://chartapi.finance.yahoo.com/instrument/1.0/' \
+                        +stock+'/chartdata;type=quote;range=1y/csv'
+
         try:
             data = []
             source_code = urllib.request.urlopen(url_to_visit).read().decode()
@@ -21,26 +22,33 @@ class StockPuller():
                 if len(split_line)==6:
                     if 'values' not in each_line:
                         data.append(each_line)
-            # Update last day
-            #share_today = Share(stock)
-            #tm = share_today.get_trade_datetime().split()[0]
-            #today_date = tm.replace("-","")
 
-            #l_date = data[-1].split(',')[0]
-            '''
-            if today_date and l_date != today_date:
+            if not data:
+                return None
+
+            # Update last day
+            share_today = Share(stock)
+            tm = share_today.get_trade_datetime().split()[0]
+            today_date = tm.replace("-","")
+
+            l_date = data[-1].split(',')[0]
+
+            if data and l_date != today_date:
                 today_close_price = share_today.get_price()
                 today_high_price = share_today.get_days_high()
                 today_low_price = share_today.get_days_low()
                 today_open_price = share_today.get_open()
                 today_volume = share_today.get_volume()
-                last_row = today_date+','+ today_close_price+','+today_high_price+','+today_low_price +','+today_open_price+','+today_volume
-                if today_close_price and today_high_price and today_open_price and today_volume:
-                    data.append(last_row) '''
+                last_row = today_date+','+ today_close_price+',' \
+                           +today_high_price+','+today_low_price +',' \
+                           +today_open_price+','+today_volume
+
+                if today_close_price and today_high_price and \
+                    today_open_price and today_volume:
+                    data.append(last_row)
 
             return data
 
         except Exception as e:
             pdb.set_trace()
             print(str(e), 'failed to organize pulled data.')
-
