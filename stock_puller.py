0dfbf46d4d4387b19e506403d3575427c16067f1
Eugene Hazan <ehazan@gmail.com>
2016-12-20 15:20:24 -0500

    Updates

--- stock_puller.py
+++ stock_puller.py
@@ -35,29 +35,32 @@ class StockPuller():
 
             # Update last day
             share_today = Share(stock)
+
+            try:
+                if share_today:
+                    tm = share_today.get_trade_datetime().split()[0]
+                    today_date = tm.replace("-","")
+
+                    l_date = data[-1].split(',')[0]
+
+                    if data and l_date < today_date:
+                        today_close_price = share_today.get_price()
+                        today_high_price = share_today.get_days_high()
+                        today_low_price = share_today.get_days_low()
+                        today_open_price = share_today.get_open()
+                        today_volume = share_today.get_volume()
+
+                        if today_close_price != None and today_high_price != None and \
+                           today_open_price != None and today_volume != None:
+
+                           last_row = today_date+','+ today_close_price+',' \
+                                   +today_high_price+','+today_low_price +',' \
+                                   +today_open_price+','+today_volume
+
+                           data.append(last_row)
+            except Exception as e:
+                print("Error occured while accessing last date ", e)
             
-            if share_today:
-                tm = share_today.get_trade_datetime().split()[0]
-                today_date = tm.replace("-","")
-
-                l_date = data[-1].split(',')[0]
-
-                if data and l_date < today_date:
-                    today_close_price = share_today.get_price()
-                    today_high_price = share_today.get_days_high()
-                    today_low_price = share_today.get_days_low()
-                    today_open_price = share_today.get_open()
-                    today_volume = share_today.get_volume()
-
-                    if today_close_price != None and today_high_price != None and \
-                       today_open_price != None and today_volume != None:
-
-                       last_row = today_date+','+ today_close_price+',' \
-                               +today_high_price+','+today_low_price +',' \
-                               +today_open_price+','+today_volume
-
-                       data.append(last_row)
-
             return data
 
         except Exception as e:
